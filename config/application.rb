65feae470626f608336e0dece6e99fcc0369c68d
1ttk2 <Tatsuki101215@gmail.com>
2022-05-26 10:54:36 +0000

    fix design

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module VRPP
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = 'Tokyo'
   end
